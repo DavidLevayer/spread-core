5b8fd9a5986cacc25a2d3dd337d1674f00ebaefa
DavidLevayer <david.levayer@gmail.com>
2019-05-13 13:36:09 +0200

    add id to resource item

--- libs/core/state/resource.state.js
+++ libs/core/state/resource.state.js
@@ -6,10 +6,10 @@ class ResourceState {
 
     constructor() {
         this.resources = [];
-        this.resources.push(new Resource('sand', 0, 10));
-        this.resources.push(new Resource('water', 0, 10));
-        this.resources.push(new Resource('food', 0, 10));
-        this.resources.push(new Resource('wood', 0, 10));
+        this.resources.push(new Resource('bcfa872f-6634-4586-8bfe-ab81dd0b627c', 'sand', 0, 10));
+        this.resources.push(new Resource('e470c5b4-8bcc-4963-bb56-9f96a1393c77', 'water', 0, 10));
+        this.resources.push(new Resource('b4506f2f-31df-47fd-a72d-dae30743c1ff', 'food', 0, 10));
+        this.resources.push(new Resource('f86bec64-891b-42c3-bec1-03929f2eaca9', 'wood', 0, 10));
     }
 
     /**
--- libs/model/resource.js
+++ libs/model/resource.js
@@ -4,11 +4,13 @@ class Resource {
 
     /**
      * 
+     * @param {uuid} id
      * @param {string} name 
      * @param {number} value 
      * @param {number} rate 
      */
-    constructor(name, value, rate) {
+    constructor(id, name, value, rate) {
+        this.id = id;
         this.name = name;
         this.value = value;
         this.rate = rate;
